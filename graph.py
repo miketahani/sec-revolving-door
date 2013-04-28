55ee2433312f6ec011c7e56cde2193501d96241b
miketahani <m.tahani@gmail.com>
2013-04-28 00:33:13 -0700

    cleanup

--- graph.py
+++ graph.py
@@ -7,7 +7,8 @@ with open('pogo_database.html') as infile:
 
 soup = bs(raw_html)
 hdr = ["full_name", "fmr_office", "fmr_title", "new_employer", "resign_date", "statement_date"]
-nodes, links = [], []
+nodes, links = [], set()
+flat_nodes = []
 for row in soup.find("tbody").findAll("tr"):
     fields = [field.text for field in row.findAll("td")]
     entry = dict(zip(hdr, fields))
@@ -24,18 +25,20 @@ for row in soup.find("tbody").findAll("tr"):
         }
     ]
     for node in new_nodes:
-        if node not in nodes: nodes.append(node)
-    links.append( (full_name, new_employer) )
+        if node not in nodes: 
+            nodes.append(node)
+            flat_nodes.append(node["name"])
+    link = sorted([full_name, new_employer])
+    links.add( tuple(link) )
 
 graph = {"nodes": nodes, "links": []}
-flat_nodes = [node["name"] for node in nodes]
 finished = []
 for node in nodes:
     connections = [link for link in links if node["name"] in link]
     for connection in connections:
         src, target = connection
-        if (src, target) in finished or (target, src) in finished: continue
-        finished += [(src, target), (target, src)]
+        if connection in finished: continue
+        finished.append(connection)
         link = {
             "source": flat_nodes.index(src),
             "target": flat_nodes.index(target)
